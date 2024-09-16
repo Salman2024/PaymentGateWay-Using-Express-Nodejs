ec38f91450b39c69d4ee02729a42c2f165bb2bd5
Salman2024 <121297968+Salman2024@users.noreply.github.com>
2024-09-16 18:32:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+const app = express()
+
+var Publishable_Key = 'pk_test_51NDQcxSEqqg3FYu2rkxvto76SK41u9sMEasMYDA4zxrPFRG322tymE6hJRnvqmNIpAB64jD5LgSu0tVa4oys468v0012mRj5GW'
+var Secret_Key = 'sk_test_51NDQcxSEqqg3FYu2P4vcOFcaPj7mP3CrLJBN8weWlS0JTfux0oZU8mvTI7VhFCtm1ILlu6mT8UL81Jsu6GuyhabJ00TrnZkcdE'
+
+const stripe = require('stripe')(Secret_Key)
+
+const port = process.env.PORT || 3000
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+	res.render('Home', {
+	key: Publishable_Key
+	})
+})
+
+app.post('/payment', function(req, res){
+
+	// Moreover you can take more details from user
+	// like Address, Name, etc from form
+	stripe.customers.create({
+		email: req.body.stripeEmail,
+		source: req.body.stripeToken,
+		name: 'Gourav Hammad',
+		address: {
+			line1: 'TC 9/4 Old MES colony',
+			postal_code: '452331',
+			city: 'Indore',
+			state: 'Madhya Pradesh',
+			country: 'India',
+		}
+	})
+	.then((customer) => {
+
+		return stripe.charges.create({
+			amount: 100,	 // Charging Rs 25
+			description: 'Web Development Product',
+			currency: 'usd',
+			customer: customer.id
+		});
+	})
+	.then((charge) => {
+		res.send("Success") // If no error occurs
+	})
+	.catch((err) => {
+		res.send(err)	 // If some error occurs
+	});
+})
+
+app.listen(port, function(error){
+	if(error) throw error
+	console.log("Server created Successfully")
+})
